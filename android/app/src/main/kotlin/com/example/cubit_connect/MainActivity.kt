4eab25e7525d349514088daaf2c0f22a52e2aa51
Taha <tahax.dev@gmail.com>
2022-06-26 19:46:59 +0430

    initial Commit

---
+++ android/app/src/main/kotlin/com/example/cubit_connect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cubit_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/cubits/color/color_state.dart
@@ -0,0 +1,23 @@
+part of 'color_cubit.dart';
+
+class ColorState extends Equatable {
+  final Color color;
+  const ColorState({required this.color});
+
+  factory ColorState.initial() {
+    return const ColorState(color: Colors.red);
+  }
+
+  @override
+  List<Object> get props => [];
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'ColorState(color : $color)';
+  }
+
+  ColorState copyWith({Color? color}) {
+    return ColorState(color: color ?? this.color);
+  }
+}
